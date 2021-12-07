cb9ea7e646835b6fb1a7f673828e9b54097f81ae
tlacloc <desneruda@gmail.com>
2021-12-06 20:29:54 -0600

    edit document untested

---
+++ include/contracts.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <eosio/name.hpp>
+
+using namespace eosio;
+
+namespace contracts
+{
+  const name gendoceditor = "gendoceditor"_n;
+  
+}
+
+namespace others
+{
+  const name contract = "contract"_n;
+}
---
+++ include/util.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <eosio/name.hpp>
+#include <eosio/asset.hpp>
+#include <contracts.hpp>
+#include <variant>
+
+using namespace eosio;
+using std::string;
+
+namespace util
+{
+	
+
+}
